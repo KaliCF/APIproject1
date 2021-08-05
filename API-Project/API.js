56ee4de8031deb3f3884370a9dd51156089c1473
KaliCF <kalifenimore@gmail.com>
2021-08-05 16:55:36 -0400

    3rd push

--- API-Project/API.js
+++ API-Project/API.js
@@ -1,11 +1,22 @@
-const baseURL = 'https://ghibliapi.herokuapp.com/vehicles/';
+const baseURL = 'https://ghibliapi.herokuapp.com/vehicles';
 
 const display = (vehicles) => {
-    let vehic1 = vehicles.results[0].name
-    let vehic2 = vehicles.results[1].name
-    let vehic3 = vehicles.results[2].name
+    let vehicleName1 = vehicles[0].name
+    let vehicleName2 = vehicles[1].name
+    let vehicleName3 = vehicles[2].name
+
+    let aDg = vehicles[0].description
+    let rW = vehicles[1].description
+    let sB = vehicles[2].description
+
+    document.getElementById('ve1').innerText = vehicleName1 + '. ' + aDg
+    document.getElementById('ve2').innerText = vehicleName2 + '. ' + rW
+    document.getElementById('ve3').innerText = vehicleName3 + '. ' + sB
 }
 
 fetch(baseURL)
     .then(response => response.json())
-    .then(data => display(vehicles))
+    .then(data => {
+        display(data)
+        console.log(data)
+    })
